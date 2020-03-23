b57c00ba9e44d2543f75b8b5d23d3806674b315f
Jonathan Reeve <jon.reeve@gmail.com>
2020-03-23 13:33:23 -0400

    clean up default.nix

--- default.nix
+++ default.nix
@@ -9,8 +9,6 @@ in
           ghcid
         ]);
     source-overrides = {
-      # numhask-space = pkgs.haskell.lib.dontCheck pkgs.haskellPackages.numhask-space;
       plotlyhs = builtins.fetchTarball "https://github.com/JonathanReeve/plotlyhs/archive/d07770b.tar.gz";
-        # pkgs.haskell.lib.callCabal2Nix 
-        };
+    };
   }
